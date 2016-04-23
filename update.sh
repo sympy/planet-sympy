4e37b4623f7f011e5f8037eb972ddcaca034c874
Ondřej Čertík <ondrej.certik@gmail.com>
2016-04-23 13:09:57 -0600

    Deploy at the end of the script
    That way most of the script will be tested by Travis (which will escape
    eventually since DEPLOY_TOKEN is empty on Travis).

--- update.sh
+++ update.sh
@@ -8,26 +8,8 @@ cd testrun
 ./rawdog -d planetsympy/ --write
 cd ..
 
-set +x
-if [ "${DEPLOY_TOKEN}" = "" ]; then
-    echo "Not deploying because DEPLOY_TOKEN is empty."
-    exit 0
-fi
-openssl aes-256-cbc -k ${DEPLOY_TOKEN} -in deploykey.enc -out deploykey -d
-set -x
-
-chmod 600 deploykey
-eval `ssh-agent -s`
-ssh-add deploykey
-
-
-if [ ! -n "$(grep "^github.com " ~/.ssh/known_hosts)" ]; then
-    mkdir ~/.ssh
-    chmod 700 ~/.ssh
-    ssh-keyscan github.com >> ~/.ssh/known_hosts
-fi
 rm -rf planet.sympy.org
-git clone git@github.com:planet-sympy/planet.sympy.org.git
+git clone https://github.com/planet-sympy/planet.sympy.org
 cd planet.sympy.org
 
 git config user.name "Docker"
@@ -40,4 +22,26 @@ cp -r ../testrun/website/* .
 echo "planet.sympy.org" > CNAME
 git add -A .
 git commit -m "${COMMIT_MESSAGE}"
-git push origin gh-pages
+
+
+echo "Deploying:"
+
+set +x
+if [ "${DEPLOY_TOKEN}" = "" ]; then
+    echo "Not deploying because DEPLOY_TOKEN is empty."
+    exit 0
+fi
+openssl aes-256-cbc -k ${DEPLOY_TOKEN} -in deploykey.enc -out deploykey -d
+set -x
+
+chmod 600 deploykey
+eval `ssh-agent -s`
+ssh-add deploykey
+
+if [ ! -n "$(grep "^github.com " ~/.ssh/known_hosts)" ]; then
+    mkdir ~/.ssh
+    chmod 700 ~/.ssh
+    ssh-keyscan github.com >> ~/.ssh/known_hosts
+fi
+
+git push git@github.com:planet-sympy/planet.sympy.org gh-pages
