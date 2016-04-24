1a7f8853d2bd8f16d334df73a20a99520f4564a7
Ondřej Čertík <ondrej.certik@gmail.com>
2016-04-24 14:31:35 -0600

    Use token

--- bin/test_travis.sh
+++ bin/test_travis.sh
@@ -4,9 +4,8 @@
 set -e
 set -x
 
-echo "TOKEN: ${GH_TOKEN2}"
 # Do *not* echo commands (so that we don't reveal GH_TOKEN)
 set +x
 
 echo "docker run -it -e DEPLOY_TOKEN=GH_TOKEN -e TRAVIS=true test/planet-sympy"
-docker run -it -e DEPLOY_TOKEN=${GH_TOKEN2} -e TRAVIS=true test/planet-sympy
+docker run -it -e DEPLOY_TOKEN=${GH_TOKEN} -e TRAVIS=true test/planet-sympy
