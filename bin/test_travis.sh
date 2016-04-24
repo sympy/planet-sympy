ac2bd52fe7fea6213e1e0d75906a420e2e7dda6e
Ondřej Čertík <ondrej.certik@gmail.com>
2016-04-24 13:02:42 -0600

    Try another name

--- bin/test_travis.sh
+++ bin/test_travis.sh
@@ -6,4 +6,4 @@ set -e
 set +x
 
 echo "docker run -it -e DEPLOY_TOKEN=GH_TOKEN -e TRAVIS=true test/planet-sympy"
-docker run -it -e DEPLOY_TOKEN=${GH_TOKEN} -e TRAVIS=true test/planet-sympy
+docker run -it -e DEPLOY_TOKEN=${GH_TOKEN2} -e TRAVIS=true test/planet-sympy
