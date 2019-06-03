e1f638d3881784cd22d420177024bfb491a735d3
Ondřej Čertík <ondrej@certik.us>
2019-06-03 14:42:20 -0600

    Use just the gith hash as a tag, add "latest" tag
    Now the "latest" tag points to the latest docker image.

--- build_image.sh
+++ build_image.sh
@@ -6,8 +6,10 @@ set -x
 docker login -u "$CI_REGISTRY_USER" -p "$CI_REGISTRY_PASSWORD" $CI_REGISTRY
 image_base_name=planet-sympy
 image_version=${CI_COMMIT_SHA:0:7}
-DNAME="$CI_REGISTRY_IMAGE:$image_base_name-$image_version"
+DNAME="$CI_REGISTRY_IMAGE:$image_version"
+DNAME2="$CI_REGISTRY_IMAGE:latest"
 
-docker build -t "$DNAME" .
+docker build -t "$DNAME" -t "$DNAME2" .
 
 docker push "$DNAME"
+docker push "$DNAME2"
